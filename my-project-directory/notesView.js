aab4f47df215d4a776a13a80f3f6343bfb9f44bf
Daisy <ysiad926@gmail.com>
2023-01-04 17:22:56 +0000

    test drive the form to add a new note

--- my-project-directory/notesView.js
+++ my-project-directory/notesView.js
@@ -2,6 +2,12 @@ class NotesView {
 constructor(model) {
   this.model = model;
   this.mainContainerEl = document.querySelector('#main-container');
+  this.buttonEl = document.querySelector('#add-note-btn')
+
+  this.buttonEl.addEventListener('click',() => {
+    const newNote = document.querySelector('#add-note-input').value;
+    this.addNewNote(newNote);
+  })
   }
 
   displayNotes() {
@@ -13,6 +19,11 @@ constructor(model) {
       noteEl.className = 'note';
       this.mainContainerEl.append(noteEl);
     })
+  }
+
+  addNewNote(newNote){
+    this.model.addNote(newNote);
+    this.displayNotes();
   }
 }
 
--- my-project-directory/notesView.test.js
+++ my-project-directory/notesView.test.js
@@ -7,18 +7,32 @@ const fs = require('fs');
 const NotesModel = require('./notesModel')
 const NotesView = require('./notesView')
 
+beforeEach(() => {
+  document.body.innerHTML = fs.readFileSync('./index.html');
+
+  model = new NotesModel();
+  view = new NotesView(model);
+})
+
 describe(NotesView,() => {
   it('display two notes',() => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-
-    const model = new NotesModel();
-    const view = new NotesView(model);
 
     model.addNote('Hi')
     model.addNote('Hello')
 
     view.displayNotes();
     
-    expect(document.querySelectorAll('div.note').length).toBe(2)
+    expect(document.querySelectorAll('div.note').length).toEqual(2)
   })
+// fill the input
+  it('add a new note',() => {
+    const input = document.querySelector('#add-note-input');
+    input.value = 'My new note';
+// click the button
+    const btn = document.querySelector('#add-note-btn')
+    btn.click();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].textContent).toEqual('My new note')
+  } )
 })
